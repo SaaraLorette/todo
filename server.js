eddc697d26f166d5e765beb82a8bead07e9e0b4d
Argo Roots <argo@roots.ee>
2017-11-22 16:10:55 +0200

    Send new todo items to server

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+
+
 const app = express()
 
-
 app.set('view engine', 'pug')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', function (req, res) {
-  let data = {
+
+
+let data = {
     title: 'ToDo',
     todoList: [
         { text: 'Piim', isDone: false },
         { text: 'Sai', isDone: true },
         { text: 'Või', isDone: false }
     ]
-  }
+}
 
-  res.render('index', data)
+app.get('/', function (req, res) {
+
+    res.render('index', data)
 })
 
+app.post('/submit_new', function (req, res) {
+    data.todoList.push({
+        text: req.body.new_todo_item,
+        isDone: false
+    })
+    res.redirect('/')
+})
+
+
+
 app.listen(process.env.PORT, process.env.IP, function () {
-  console.log('Server listening on port ' + process.env.PORT)
+    console.log('Server listening on port ' + process.env.PORT)
 })
