fd6121d3bc6886ec0e935919647019324d7cc02b
Argo Roots <argo@roots.ee>
2017-11-22 15:27:16 +0200

    Show todoList

--- server.js
+++ server.js
@@ -6,20 +6,16 @@ app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
   let data = {
-    title: 'Hey',
-    message: 'Hello there!'
+    title: 'ToDo',
+    todoList: [
+        { text: 'Piim', isDone: false },
+        { text: 'Sai', isDone: true },
+        { text: 'Või', isDone: false }
+    ]
   }
+
   res.render('index', data)
 })
-
-app.get('/test', function (req, res) {
-  let data = {
-    title: 'Test',
-    message: 'Test!'
-  }
-  res.render('index', data)
-})
-
 
 app.listen(process.env.PORT, process.env.IP, function () {
   console.log('Server listening on port ' + process.env.PORT)
